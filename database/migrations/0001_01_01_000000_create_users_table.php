2d8edd7e486616fc23da58594353edf87c0a7c0b
Kidus <kidusm7110@gmail.com>
2025-10-14 20:37:09 +0300

    Update User migration file

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['admin', 'manager', 'receptionist', 'client'])->default('client');
+            $table->foreignId('hotel_id')->nullable()->constrained()->onDelete('cascade');
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
 
