155780b968c4e3b184cfeae1ce8374cd9c62c87e
Kidus <kidusm7110@gmail.com>
2025-10-14 20:59:30 +0300

    Create seeder and factory for user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin', 'manager', 'receptionist', 'client'])->default('client');
-            $table->foreignId('hotel_id')->nullable()->constrained()->onDelete('cascade');
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
